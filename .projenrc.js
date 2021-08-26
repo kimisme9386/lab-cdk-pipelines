fec179c23fd634b8735bfd5b29b6ae0a003595fc
Chris Yang <kimisme9386@gmail.com>
2021-08-26 12:01:23 +0800

    chore: disable github workflow

--- .projenrc.js
+++ .projenrc.js
@@ -1,15 +1,29 @@
-const { AwsCdkTypeScriptApp } = require('projen');
+const { AwsCdkTypeScriptApp, DependenciesUpgradeMechanism } = require('projen');
 const project = new AwsCdkTypeScriptApp({
   cdkVersion: '1.95.2',
   defaultReleaseBranch: 'main',
   name: 'lab-cdk-pipelines',
-
-  // cdkDependencies: undefined,        /* Which AWS CDK modules (those that start with "@aws-cdk/") this app uses. */
-  // deps: [],                          /* Runtime dependencies of this module. */
-  // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],                       /* Build dependencies for this module. */
-  // packageName: undefined,            /* The "name" in package.json. */
-  // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
-  // release: undefined,                /* Add release management to this project. */
+  releaseWorkflow: false,
+  buildWorkflow: false,
+  stale: false,
+  depsUpgrade: DependenciesUpgradeMechanism.NONE,
+  cdkDependencies: [
+    '@aws-cdk/pipelines',
+    '@aws-cdk/aws-events-targets',
+    '@aws-cdk/aws-codebuild',
+    '@aws-cdk/aws-codepipeline',
+    '@aws-cdk/aws-iam',
+    '@aws-cdk/aws-lambda',
+    '@aws-cdk/aws-s3',
+    '@aws-cdk/core',
+    '@aws-cdk/aws-ecr',
+  ],
+  devDeps: [
+    '@types/aws-lambda@^8.10.72',
+    'eslint-config-prettier',
+    'eslint-plugin-prettier',
+    '@types/js-yaml@^3.12.5',
+  ],
+  deps: ['js-yaml@^3.14.1'],
 });
 project.synth();
